653c83d0a181c147e34956678facc8a4b55ac2fc
Rayane Carmelia Custodio <raayanecustodio@gmail.com>
2026-05-19 00:00:09 -0300

    Adicionando nova estrutura para receber dados do pocketbase no products

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -1,9 +1,9 @@
 class Product {
-  int id;
+  final String id; 
   String nomeProduto;
   int quantidade;
   double preco;
-  int? supplierId; 
+  String? supplierId; 
 
   Product({
     required this.id,
@@ -12,4 +12,25 @@ class Product {
     required this.preco,
     this.supplierId, 
   });
+
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'] ?? '',
+      nomeProduto: json['nomeProduto'] ?? '',
+      quantidade: (json['quantidade'] ?? 0) as int, 
+      preco: (json['preco'] ?? 0.0).toDouble(), // Garante que vire double sem quebrar
+      supplierId: json['supplierId'], // Pode vir nulo se não houver fornecedor
+    );
+  }
+
+
+  Map<String, dynamic> toJson() {
+    return {
+      'nomeProduto': nomeProduto,
+      'quantidade': quantidade,
+      'preco': preco,
+      'supplierId': supplierId,
+    };
+  }
 }
