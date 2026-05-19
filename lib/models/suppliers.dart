852d4876f140eef6a9eccc0559d4a42d02c2e389
Rayane Carmelia Custodio <raayanecustodio@gmail.com>
2026-05-19 00:07:53 -0300

    Adicionando nova estrutura para receber dados do pocketbase no supliers

--- lib/models/suppliers.dart
+++ lib/models/suppliers.dart
@@ -1,8 +1,32 @@
 class Suppliers {
-  int id;
+  final String id;
   String nome;
   String telefone;
   String email;
 
-  Suppliers({required this.id,required this.nome, required this.telefone, required this.email});
+  Suppliers({
+    required this.id,
+    required this.nome, 
+    required this.telefone, 
+    required this.email,
+  });
+
+
+  factory Suppliers.fromJson(Map<String, dynamic> json) {
+    return Suppliers(
+      id: json['id'] ?? '',
+      nome: json['nome'] ?? '',
+      telefone: json['telefone'] ?? '',
+      email: json['email'] ?? '',
+    );
+  }
+
+
+  Map<String, dynamic> toJson() {
+    return {
+      'nome': nome,
+      'telefone': telefone,
+      'email': email,
+    };
+  }
 }
