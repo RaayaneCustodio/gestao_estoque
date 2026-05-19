875752f9fcbf871089c571b64388b41f127d2059
Rayane Carmelia Custodio <raayanecustodio@gmail.com>
2026-05-18 23:47:29 -0300

    Customers models ajustado para receber o banco de dados pocketbase

--- lib/models/customers.dart
+++ lib/models/customers.dart
@@ -1,7 +1,32 @@
 class Customers {
-  int id;
+  final String id;
   String nome;
   String telefone;
   String email;
-  Customers({required this.id, required this.nome, required this.telefone, required this.email});
+
+  Customers({
+    required this.id, 
+    required this.nome, 
+    required this.telefone, 
+    required this.email,
+  });
+
+
+  factory Customers.fromJson(Map<String, dynamic> json) {
+    return Customers(
+      id: json['id'] ?? '',
+      nome: json['nome'] ?? '',
+      telefone: json['telefone'] ?? '',
+      email: json['email'] ?? '',
+    );
+  }
+
+
+  Map<String, dynamic> toJson() {
+    return {
+      'nome': nome,
+      'telefone': telefone,
+      'email': email,
+    };
+  }
 }
