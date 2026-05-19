21db7cfe5f12aa8d93006e83056148d0907461fb
Rayane Carmelia Custodio <raayanecustodio@gmail.com>
2026-05-19 00:03:14 -0300

    Adicionando nova estrutura para receber dados do pocketbase no sale

--- lib/models/sale.dart
+++ lib/models/sale.dart
@@ -1,7 +1,7 @@
 class Sale {
-  int id;
-  int customerId;
-  int productId;
+  final String id; 
+  String customerId; 
+  String productId;
   int quantidade;
   double precoUnitario;
   DateTime data;
@@ -14,4 +14,27 @@ class Sale {
     required this.precoUnitario,
     required this.data,
   });
+
+
+  factory Sale.fromJson(Map<String, dynamic> json) {
+    return Sale(
+      id: json['id'] ?? '',
+      customerId: json['customerId'] ?? '',
+      productId: json['productId'] ?? '',
+      quantidade: (json['quantidade'] ?? 0) as int,
+      precoUnitario: (json['precoUnitario'] ?? 0.0).toDouble(),
+      data: json['data'] != null ? DateTime.parse(json['data']) : DateTime.now(),
+    );
+  }
+
+
+  Map<String, dynamic> toJson() {
+    return {
+      'customerId': customerId,
+      'productId': productId,
+      'quantidade': quantidade,
+      'precoUnitario': precoUnitario,
+      'data': data.toIso8601String(),
+    };
+  }
 }
